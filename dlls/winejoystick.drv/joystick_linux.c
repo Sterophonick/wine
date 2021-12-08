d7967cc25c328ed8a004d5eec14adf30733862f8
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2021-12-07 19:55:32 -0700

    sdl winejoystick initial work

--- dlls/winejoystick.drv/joystick_linux.c
+++ dlls/winejoystick.drv/joystick_linux.c
@@ -69,6 +69,10 @@
 
 #include "wine/unicode.h"
 
+#ifdef HAVE_SDL_H
+# include <SDL.h>
+#endif
+
 WINE_DEFAULT_DEBUG_CHANNEL(joystick);
 
 #define MAXJOYSTICK (JOYSTICKID2 + 30)
@@ -76,17 +80,8 @@ WINE_DEFAULT_DEBUG_CHANNEL(joystick);
 typedef struct tagWINE_JSTCK {
     int		joyIntf;
     BOOL        in_use;
-    /* Some extra info we need to make this actually work under the
-       Linux 2.2 event api.
-       First of all, we cannot keep closing and reopening the device file -
-       that blows away the state of the stick device, and we lose events. So, we
-       need to open the low-level device once, and close it when we are done.
 
-       Secondly, the event API only gives us what's changed. However, Windows apps
-       want the whole state every time, so we have to cache the data.
-    */
-
-    int         dev; /* Linux level device file descriptor */
+    int         id; //SDL2 GameController ID
     int         x;
     int         y;
     int         z;
@@ -100,6 +95,7 @@ typedef struct tagWINE_JSTCK {
 } WINE_JSTCK;
 
 static	WINE_JSTCK	JSTCK_Data[MAXJOYSTICK];
+SDL_GameController *joysticks[MAXJOYSTICK] = NULL;
 
 /**************************************************************************
  * 				JSTCK_drvGet			[internal]
@@ -126,7 +122,7 @@ LRESULT driver_open(LPSTR str, DWORD dwIntf)
 	return 0;
 
     JSTCK_Data[dwIntf].joyIntf = dwIntf;
-    JSTCK_Data[dwIntf].dev = -1;
+    JSTCK_Data[dwIntf].id = -1;
     JSTCK_Data[dwIntf].in_use = TRUE;
     return (LRESULT)&JSTCK_Data[dwIntf];
 }
@@ -143,9 +139,10 @@ LRESULT driver_close(DWORD_PTR dwDevID)
     jstck->in_use = FALSE;
     if (jstck->dev > 0)
     {
-       close(jstck->dev);
+        SDL_GameControllerClose(joysticks[jstck->dev]);
        jstck->dev = 0;
     }
+
     return 1;
 }
 
@@ -166,43 +163,25 @@ static	int	JSTCK_OpenDevice(WINE_JSTCK* jstick)
     static DWORD last_attempt;
     DWORD now;
 
-    if (jstick->dev > 0)
-      return jstick->dev;
+    if (jstick->id > 0)
+      return jstick->id;
 
     now = GetTickCount();
     if (now - last_attempt < 2000)
       return -1;
     last_attempt = now;
 
-    flags = O_RDONLY | O_NONBLOCK;
-
-    /* The first joystick may not be at /dev/input/js0, find the correct
-     * first or second device. For example the driver for XBOX 360 wireless
-     * receiver creates entries starting at 20.
-     */
-    for (found_ix = i = 0; i < MAXJOYSTICK; i++) {
-        sprintf(buf, JOYDEV_NEW, i);
-        if ((fd = open(buf, flags)) < 0) {
-            sprintf(buf, JOYDEV_OLD, i);
-            if ((fd = open(buf, flags)) < 0)
-                continue;
-        }
-
-        if (found_ix++ == jstick->joyIntf)
-        {
-            TRACE("Found joystick[%d] at %s\n", jstick->joyIntf, buf);
-            jstick->dev = fd;
-            last_attempt = 0;
+    if (SDL_IsGameController(jstck->id)) {
+        joysticks[jstck->id] = SDL_GameControllerOpen(jstck->id);
+        if (joysticks[jstck->id]) {
+            fprintf("Located controller device at SDL:%i", jstick->id);
             break;
+        } else {
+            fprintf(stderr, "Could not open gamecontroller %i: %s\n", jstick->id, SDL_GetError());
         }
-
-        close(fd);
     }
 
-    if (jstick->dev > 0)
-        ioctl(jstick->dev, JSIOCGAXMAP, jstick->axesMap);
-
-    return jstick->dev;
+    return jstick->id;
 }
 
 
@@ -223,86 +202,6 @@ LRESULT driver_joyGetDevCaps(DWORD_PTR dwDevID, LPJOYCAPSW lpCaps, DWORD dwSize)
 	return MMSYSERR_NODRIVER;
 
     if ((dev = JSTCK_OpenDevice(jstck)) < 0) return JOYERR_PARMS;
-    ioctl(dev, JSIOCGAXES, &nrOfAxes);
-    ioctl(dev, JSIOCGBUTTONS, &nrOfButtons);
-    ioctl(dev, JSIOCGVERSION, &driverVersion);
-    ioctl(dev, JSIOCGNAME(sizeof(identString)), identString);
-    TRACE("Driver: 0x%06x, Name: %s, #Axes: %d, #Buttons: %d\n",
-	  driverVersion, identString, nrOfAxes, nrOfButtons);
-    lpCaps->wMid = MM_MICROSOFT;
-    lpCaps->wPid = MM_PC_JOYSTICK;
-    MultiByteToWideChar(CP_UNIXCP, 0, identString, -1, lpCaps->szPname, MAXPNAMELEN);
-    lpCaps->szPname[MAXPNAMELEN-1] = '\0';
-    lpCaps->wXmin = 0;
-    lpCaps->wXmax = 0xFFFF;
-    lpCaps->wYmin = 0;
-    lpCaps->wYmax = 0xFFFF;
-    lpCaps->wZmin = 0;
-    lpCaps->wZmax = (nrOfAxes >= 3) ? 0xFFFF : 0;
-#ifdef BODGE_THE_HAT
-    /* Half-Life won't allow you to map an axis event to things like
-       "next weapon" and "use". Linux reports the hat on my stick as
-       axis U and V. So, IFF BODGE_THE_HAT is defined, lie through our
-       teeth and say we have 32 buttons, and we will map the axes to
-       the high buttons. Really, perhaps this should be a registry entry,
-       or even a parameter to the Linux joystick driver (which would completely
-       remove the need for this.)
-    */
-    lpCaps->wNumButtons = 32;
-#else
-    lpCaps->wNumButtons = nrOfButtons;
-#endif
-    if (dwSize == sizeof(JOYCAPSW)) {
-	/* complete 95 structure */
-	lpCaps->wRmin = 0;
-	lpCaps->wRmax = 0xFFFF;
-	lpCaps->wUmin = 0;
-	lpCaps->wUmax = 0xFFFF;
-	lpCaps->wVmin = 0;
-	lpCaps->wVmax = 0xFFFF;
-	lpCaps->wMaxAxes = 6; /* same as MS Joystick Driver */
-	lpCaps->wNumAxes = 0; /* nr of axes in use */
-	lpCaps->wMaxButtons = 32; /* same as MS Joystick Driver */
-	lpCaps->szRegKey[0] = 0;
-	lpCaps->szOEMVxD[0] = 0;
-	lpCaps->wCaps = 0;
-        for (i = 0; i < nrOfAxes; i++) {
-	    switch (jstck->axesMap[i]) {
-	    case 0: /* X */
-	    case 1: /* Y */
-	    case 8: /* Wheel */
-	    case 9: /* Gas */
-		lpCaps->wNumAxes++;
-		break;
-	    case 2: /* Z */
-	    case 6: /* Throttle */
-	    case 10: /* Brake */
-		lpCaps->wNumAxes++;
-		lpCaps->wCaps |= JOYCAPS_HASZ;
-		break;
-	    case 5: /* Rz */
-	    case 7: /* Rudder */
-		lpCaps->wNumAxes++;
-		lpCaps->wCaps |= JOYCAPS_HASR;
-		break;
-	    case 3: /* Rx */
-		lpCaps->wNumAxes++;
-		lpCaps->wCaps |= JOYCAPS_HASU;
-		break;
-	    case 4: /* Ry */
-		lpCaps->wNumAxes++;
-		lpCaps->wCaps |= JOYCAPS_HASV;
-		break;
-	    case 16: /* Hat 0 X */
-	    case 17: /* Hat 0 Y */
-		lpCaps->wCaps |= JOYCAPS_HASPOV | JOYCAPS_POV4DIR;
-		/* TODO: JOYCAPS_POVCTS handling */
-		break;
-	    default:
-		WARN("Unknown axis %hhu(%u). Skipped.\n", jstck->axesMap[i], i);
-	    }
-	}
-    }
 
     return JOYERR_NOERROR;
 }
@@ -312,129 +211,6 @@ LRESULT driver_joyGetDevCaps(DWORD_PTR dwDevID, LPJOYCAPSW lpCaps, DWORD dwSize)
  */
 LRESULT driver_joyGetPosEx(DWORD_PTR dwDevID, LPJOYINFOEX lpInfo)
 {
-    WINE_JSTCK*		jstck;
-    int			dev;
-    struct js_event 	ev;
-
-    if ((jstck = JSTCK_drvGet(dwDevID)) == NULL)
-	return MMSYSERR_NODRIVER;
-
-    if ((dev = JSTCK_OpenDevice(jstck)) < 0) return JOYERR_PARMS;
-
-    while ((read(dev, &ev, sizeof(struct js_event))) > 0) {
-	if (ev.type == (JS_EVENT_AXIS)) {
-	    switch (jstck->axesMap[ev.number]) {
-	    case 0: /* X */
-	    case 8: /* Wheel */
-		jstck->x = ev.value;
-		break;
-	    case 1: /* Y */
-	    case 9: /* Gas */
-		jstck->y = ev.value;
-		break;
-	    case 2: /* Z */
-	    case 6: /* Throttle */
-	    case 10: /* Brake */
-		jstck->z = ev.value;
-		break;
-	    case 5: /* Rz */
-	    case 7: /* Rudder */
-		jstck->r = ev.value;
-		break;
-	    case 3: /* Rx */
-		jstck->u = ev.value;
-		break;
-	    case 4: /* Ry */
-		jstck->v = ev.value;
-		break;
-	    case 16: /* Hat 0 X */
-		jstck->pov_x = ev.value;
-		break;
-	    case 17: /* Hat 0 Y */
-		jstck->pov_y = ev.value;
-		break;
-	    default:
-		FIXME("Unknown joystick event '%d'\n", ev.number);
-	    }
-	} else if (ev.type == (JS_EVENT_BUTTON)) {
-	    if (ev.value) {
-		    jstck->buttons |= (1 << ev.number);
-		    /* FIXME: what to do for this field when
-		     * multiple buttons are depressed ?
-		     */
-		    if (lpInfo->dwFlags & JOY_RETURNBUTTONS)
-                       lpInfo->dwButtonNumber = ev.number + 1;
-		}
-             else
-               jstck->buttons  &= ~(1 << ev.number);
-	}
-    }
-    /* EAGAIN is returned when the queue is empty */
-    if (errno != EAGAIN) {
-	/* FIXME: error should not be ignored */
-	ERR("Error while reading joystick state (%s)\n", strerror(errno));
-    }
-    /* Now, copy the cached values into Window's structure... */
-    if (lpInfo->dwFlags & JOY_RETURNBUTTONS)
-       lpInfo->dwButtons = jstck->buttons;
-    if (lpInfo->dwFlags & JOY_RETURNX)
-       lpInfo->dwXpos   = jstck->x + 32767;
-    if (lpInfo->dwFlags & JOY_RETURNY)
-       lpInfo->dwYpos   = jstck->y + 32767;
-    if (lpInfo->dwFlags & JOY_RETURNZ)
-       lpInfo->dwZpos   = jstck->z + 32767;
-    if (lpInfo->dwFlags & JOY_RETURNR)
-       lpInfo->dwRpos   = jstck->r + 32767;
-# ifdef BODGE_THE_HAT
-    else if (lpInfo->dwFlags & JOY_RETURNBUTTONS)
-    {
-         if (jstck->r > 0)
-            lpInfo->dwButtons |= 1<<7;
-         else if (jstck->r < 0)
-            lpInfo->dwButtons |= 1<<8;
-    }
-# endif
-    if (lpInfo->dwFlags & JOY_RETURNU)
-	lpInfo->dwUpos   = jstck->u + 32767;
-# ifdef BODGE_THE_HAT
-    else if (lpInfo->dwFlags & JOY_RETURNBUTTONS)
-    {
-       if (jstck->u > 0)
-          lpInfo->dwButtons |= 1<<9;
-        else if (jstck->u < 0)
-          lpInfo->dwButtons |= 1<<10;
-    }
-# endif
-    if (lpInfo->dwFlags & JOY_RETURNV)
-       lpInfo->dwVpos   = jstck->v + 32767;
-    if (lpInfo->dwFlags & JOY_RETURNPOV) {
-	if (jstck->pov_y > 0) {
-	    if (jstck->pov_x < 0)
-		lpInfo->dwPOV = 22500; /* SW */
-	    else if (jstck->pov_x > 0)
-		lpInfo->dwPOV = 13500; /* SE */
-	    else
-		lpInfo->dwPOV = 18000; /* S, JOY_POVBACKWARD */
-	} else if (jstck->pov_y < 0) {
-	    if (jstck->pov_x < 0)
-		lpInfo->dwPOV = 31500; /* NW */
-	    else if (jstck->pov_x > 0)
-		lpInfo->dwPOV = 4500; /* NE */
-	    else
-		lpInfo->dwPOV = 0; /* N, JOY_POVFORWARD */
-	} else if (jstck->pov_x < 0)
-	    lpInfo->dwPOV = 27000; /* W, JOY_POVLEFT */
-	else if (jstck->pov_x > 0)
-	    lpInfo->dwPOV = 9000; /* E, JOY_POVRIGHT */
-	else
-	    lpInfo->dwPOV = JOY_POVCENTERED; /* Center */
-    }
-
-    TRACE("x: %d, y: %d, z: %d, r: %d, u: %d, v: %d, buttons: 0x%04x, flags: 0x%04x (fd %d)\n",
-	  lpInfo->dwXpos, lpInfo->dwYpos, lpInfo->dwZpos,
-	  lpInfo->dwRpos, lpInfo->dwUpos, lpInfo->dwVpos,
-	  lpInfo->dwButtons, lpInfo->dwFlags, dev
-         );
 
     return JOYERR_NOERROR;
 }
@@ -444,20 +220,6 @@ LRESULT driver_joyGetPosEx(DWORD_PTR dwDevID, LPJOYINFOEX lpInfo)
  */
 LRESULT driver_joyGetPos(DWORD_PTR dwDevID, LPJOYINFO lpInfo)
 {
-    JOYINFOEX	ji;
-    LONG	ret;
-
-    memset(&ji, 0, sizeof(ji));
-
-    ji.dwSize = sizeof(ji);
-    ji.dwFlags = JOY_RETURNX | JOY_RETURNY | JOY_RETURNZ | JOY_RETURNBUTTONS;
-    ret = driver_joyGetPosEx(dwDevID, &ji);
-    if (ret == JOYERR_NOERROR)	{
-	lpInfo->wXpos    = ji.dwXpos;
-	lpInfo->wYpos    = ji.dwYpos;
-	lpInfo->wZpos    = ji.dwZpos;
-	lpInfo->wButtons = ji.dwButtons;
-    }
 
     return ret;
 }
